48121a41b5eb827b2414ad6b53a5ddeb7cf7ba81
Dylan Prinsloo <dp261@student.london.ac.uk>
2025-08-05 11:16:34 +0200

    Update emails.js

--- emails.js
+++ emails.js
@@ -1,5 +1,5 @@
-// amds4@student.london.ac.uk
-// dp261@student.london.ac.uk
-// kf131@student.london.ac.uk
-// crdb1@student.london.ac.uk
-// 
+// amds4@student.london.ac.uk (Silvam Alexsandra)
+// dp261@student.london.ac.uk (Dylan Prinsloo
+// kf131@student.london.ac.uk (Ken Fit)
+// crdb1@student.london.ac.uk (Chey DB)
+// kic4@student.london.ac.uk (Ian Chow)
